4f34c612bfe0535973135fef7dee53d96d935cb2
Thanabodee <96070446+TeammyNo1@users.noreply.github.com>
2022-04-19 16:30:37 +0700

    Update Project 1

---
+++ Final Project OOP.Parking/Final Project OOP.Parking/Form1.cs
@@ -0,0 +1,10 @@
+namespace Final_Project_OOP.Parking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Final Project OOP.Parking/Final Project OOP.Parking/Program.cs
@@ -0,0 +1,15 @@
+namespace Final_Project_OOP.Parking
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
